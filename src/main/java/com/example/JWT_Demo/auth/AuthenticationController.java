610151e198eac7662af7893822f84974030fc319
tantd9 <tranduytan.220300@gmail.com>
2024-06-22 22:20:51 +0700

    update project

--- src/main/java/com/example/JWT_Demo/auth/AuthenticationController.java
+++ src/main/java/com/example/JWT_Demo/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.example.JWT_Demo.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,21 +15,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-	
-	 @Autowired
-	  private  AuthenticationService service;
 
-	  @PostMapping("/register")
-	  public ResponseEntity<AuthenticationResponse> register(
-	      @RequestBody RegisterRequest request
-	  ) {
-	    return ResponseEntity.ok(service.register(request));
-	  }
-	  @PostMapping("/authenticate")
-	  public ResponseEntity<AuthenticationResponse> authenticate(
-	      @RequestBody AuthenticationRequest request
-	  ) {
-	    return ResponseEntity.ok(service.authenticate(request));
-	  }
-	
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping(value = "/authenticates", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
 }
+//
